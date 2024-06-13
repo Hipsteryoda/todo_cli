b9d91dd9efd5cf4c7459ef37f70f6f166ea9ee46
Hipsteryoda <ksmith952913@gmail.com>
2024-06-13 16:51:29 -0700

    feat: added complete functionality; -c --complete

--- todo.py
+++ todo.py
@@ -8,6 +8,13 @@ from config import TODO_FILE_PATH
 # Allow for flexible tagging (e.g. -f <tag> <value>; -f due 2024-06-10)
 # Add a list option
 
+def writeToFile(lines, file):
+    file.seek(0)                        # set the pointer to the 0th line
+    file.truncate()                     # clear the whole file to be rewritten
+    for line in lines:
+        file.write(line)
+    file.close()
+
 def add(task):
     with open(TODO_FILE_PATH, 'a') as file:
         file.write("\n- [ ] " + task)
@@ -24,14 +31,21 @@ def remove(idx):
     with open(TODO_FILE_PATH, 'r+') as file:
         lines = file.readlines()
         del lines[idx]
-        file.seek(0)                        # set the pointer to the 0th line
-        file.truncate()                     # clear the whole file to be rewritten
-        for line in lines:
-            file.write(line)
+        writeToFile(lines, file)
+
+def complete(idx):
+    with open(TODO_FILE_PATH, 'r+') as file:
+        lines = file.readlines()
+        print(lines)
+        lines[idx].replace('[ ]', '[x]')
+        print(lines[idx])
+        # print(lines)
+        # writeToFile(lines, file)
 
 try:
     parser = argparse.ArgumentParser(description = "Manages tasks in todo.md")
     parser.add_argument("-a", "--add", metavar='TASK', help="Add task")
+    parser.add_argument("-c", "--complete", type=int, metavar="LINE_NUMBER", help="Complete task at linenumber IDX")
     parser.add_argument("-l", "--list", action='store_true', help="List tasks")
     parser.add_argument("-r", "--remove", type=int, metavar="LINE_NUMBER", help="Remove line at linenumber IDX")
     args = parser.parse_args()
@@ -42,6 +56,8 @@ try:
         list()
     elif args.remove:
         remove(args.remove)
+    elif args.complete:
+        remove(args.complete)
 
 except Exception as e:
     print(e)
