2d5488791326102d8d1baec04789d579e6b8fa32
Hipsteryoda <ksmith952913@gmail.com>
2024-06-13 15:28:21 -0700

    feat: added remove function; -r

--- todo.py
+++ todo.py
@@ -18,16 +18,30 @@ def list():
         lines = file.readlines()
         for line in lines:
             print(line)
+        file.close()
+
+def remove(idx):
+    with open(TODO_FILE_PATH, 'r+') as file:
+        lines = file.readlines()
+        del lines[idx]
+        file.seek(0)                        # set the pointer to the 0th line
+        file.truncate()                     # clear the whole file to be rewritten
+        for line in lines:
+            file.write(line)
+
 try:
     parser = argparse.ArgumentParser(description = "Manages tasks in todo.md")
-    parser.add_argument("-a", "--add", metavar='TASK', help = "Add task")
-    parser.add_argument("-l", "--list", action='store_true', help = "List tasks")
+    parser.add_argument("-a", "--add", metavar='TASK', help="Add task")
+    parser.add_argument("-l", "--list", action='store_true', help="List tasks")
+    parser.add_argument("-r", "--remove", type=int, metavar="LINE_NUMBER", help="Remove line at linenumber IDX")
     args = parser.parse_args()
 
     if args.add:
         add(args.add)
     elif args.list:
         list()
+    elif args.remove:
+        remove(args.remove)
 
 except Exception as e:
     print(e)
