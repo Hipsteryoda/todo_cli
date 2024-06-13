3308678b73bc3015bc7e18983b0fd85b071a6bb9
Hipsteryoda <ksmith952913@gmail.com>
2024-06-13 15:02:21 -0700

    feat: added list functionality -l

--- todo.py
+++ todo.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 
 import argparse
+from itertools import islice
 from config import TODO_FILE_PATH
 
 # TODO:
-# Add task function
 # Allow for flexible tagging (e.g. -f <tag> <value>; -f due 2024-06-10)
+# Add a list option
 
 def add(task):
     with open(TODO_FILE_PATH, 'a') as file:
         file.write("\n- [ ] " + task)
         file.close()
 
+def list():
+    with open(TODO_FILE_PATH, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            print(line)
 try:
     parser = argparse.ArgumentParser(description = "Manages tasks in todo.md")
-    parser.add_argument("-a", "--add", help = "Add task")
+    parser.add_argument("-a", "--add", metavar='TASK', help = "Add task")
+    parser.add_argument("-l", "--list", action='store_true', help = "List tasks")
     args = parser.parse_args()
 
     if args.add:
         add(args.add)
+    elif args.list:
+        list()
 
 except Exception as e:
     print(e)
