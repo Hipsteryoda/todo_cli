0ea02fdeeecef86c764527e1069c3cf30cf6560e
Hipsteryoda <ksmith952913@gmail.com>
2024-06-17 15:58:23 -0700

    help menu cleanup

--- todo.py
+++ todo.py
@@ -9,7 +9,7 @@ try:
     parser.add_argument("-c", "--complete", type=int, metavar="LINE_NUMBER", help="Complete task at linenumber IDX")
     parser.add_argument("-l", "--list", action='store_true', help="List tasks")
     parser.add_argument("-r", "--remove", type=int, metavar="LINE_NUMBER", help="Remove line at linenumber IDX")
-    parser.add_argument("-t", "--tag", metavar="<KEY> <VALUE>", nargs=3, help="Adds a tag with a value to a given line number")
+    parser.add_argument("-t", "--tag", nargs=3, metavar="", help="<LINE> <KEY> <VALUE> Adds a tag with a value to a given line number")
     args = parser.parse_args()
 
     if args.add:
@@ -22,7 +22,6 @@ try:
         actionManager.complete(args.complete)
     elif args.tag:
         actionManager.tag(int(args.tag[0]), args.tag[1], args.tag[2])
-        # print(args.tag)
 
 except Exception as e:
     print(e)
