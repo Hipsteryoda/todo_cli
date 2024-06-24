cae80d551bd65b529d225e7ea4d25ef95fc2f537
Hipsteryoda <ksmith952913@gmail.com>
2024-06-23 19:16:40 -0700

    added due date and priority functionality

--- actionManager/actionManager.py
+++ actionManager/actionManager.py
@@ -2,33 +2,44 @@ from config import TODO_FILE_PATH
 from actionManager.db import connect, execute, commit, close, fetchall
 from datetime import datetime
 
-def add(task):
+def add(task, due_date=None, priority=None):
     # use sqlite3 to insert into db
     now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S.%f") 
     db = connect()
-    execute(db, f"""INSERT INTO tasks (task, completed, created_date) 
-            VALUES ('{task}', 0, '{now}');""")
+    execute(db, f"""INSERT INTO tasks (task
+                    , completed
+                    , created_date
+                    , due_date
+                    , priority) 
+                VALUES ('{task}'
+                    , 0
+                    , '{now}'
+                    , '{due_date}'
+                    , '{priority}');""")
     commit(db)
     close(db)
     list()
 
 def list():
-    print("ID\tTASK")
+    print("ID\tDUE DATE\tPRIORITY\tTASK")
     db = connect()
     cursor = db.cursor()
-    res = cursor.execute("""SELECT id, task 
+    res = cursor.execute("""SELECT id, due_date, priority, task 
                         FROM tasks
-                        WHERE completed = 0""")
+                        WHERE completed = 0;""")
     rows = res.fetchall()
     close(db)
     syncToFile()
     for row in rows:
-        print(f"{row[0]}\t{row[1]}")
+        print(f"{row[0]}\t{row[1]}\t{row[2]}\t{row[3]}")
 
 def complete(idx):
     # find the task in the database and set completed to 1
+    completed_date = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S.%f")
     db = connect()
-    execute(db, f"UPDATE tasks SET completed = 1 WHERE id = {idx};")
+    execute(db, f"""UPDATE tasks 
+                SET completed = 1, completed_date = '{completed_date}' 
+                WHERE id = {idx};""")
     commit(db)
     close(db)
     list()
@@ -38,7 +49,7 @@ def syncToFile():
     cursor = db.cursor()
     res = cursor.execute("""SELECT task 
                         FROM tasks
-                        WHERE completed = 0""")
+                        WHERE completed = 0;""")
     rows = res.fetchall()
     close(db)
     with open(TODO_FILE_PATH, 'w') as f:
