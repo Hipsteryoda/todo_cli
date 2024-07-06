adfd97ee7787a687a9425f44470ec69a9c69083e
Hipsteryoda <ksmith952913@gmail.com>
2024-07-06 15:21:29 -0700

    fix: timer now counts down correctly

--- pomodoro/timer.py
+++ pomodoro/timer.py
@@ -4,23 +4,24 @@ from actionManager.bcolors import bcolors
 # TODO: Make a Timer class that allows you to start and stop the timer, etc.
 class Timer:
     def __init__(self, current_task, minutes=25):
-        self.minutes_remaining = minutes - 1 
-        self.seconds_remaining = 60 
+        self.timer_minutes = minutes
+        self.timer_seconds = 60
+        self.minutes_remaining = self.timer_minutes 
+        self.seconds_remaining = self.timer_seconds
         self.current_task = current_task
 
     def start(self):
         try:
             # print task and time, clearing screen each second
-            for i in range(1, self.minutes_remaining):
-                # self.minutes_remaining = self.minutes_remaining
-                for j in range(1, self.seconds_remaining + 1):
-                    self.seconds_remaining = self.seconds_remaining - 1
+            for i in range(1, self.timer_minutes + 1):
+                self.minutes_remaining = self.timer_minutes - i
+                for j in range(1, self.timer_seconds + 1):
+                    self.seconds_remaining = self.timer_seconds - j
                     # clear screen
                     print("\033[2J\033[1;1H")
                     print(f"{bcolors.UNDERLINE}{self.current_task}{bcolors.ENDC}")
                     print(f"Time left: {self.minutes_remaining}:{self.seconds_remaining:02}")
                     time.sleep(1)
-                self.minutes_remaining = self.minutes_remaining - 1
             print("Time's up!")
             return 1
         except KeyboardInterrupt:
